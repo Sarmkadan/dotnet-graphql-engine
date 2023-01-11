62b2ca2d9897416c9d8abb2f30f4f5b5a8abae42
Vladyslav Zaiets <rutova2@gmail.com>
2023-01-11 17:56:03 +0200

    feat: defensive checks around ValidationHelper.

---
+++ src/Common/Utilities/ValidationHelperValidation.cs
@@ -0,0 +1,167 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text;
+
+namespace GraphQLEngine.Common.Utilities;
+
+/// <summary>
+/// Validation utilities for ValidationHelper static class
+/// </summary>
+public static class ValidationHelperValidation
+{
+    /// <summary>
+    /// Validates all public validation methods of ValidationHelper and returns human-readable problems
+    /// </summary>
+    /// <returns>List of validation problems, empty if valid</returns>
+    public static IReadOnlyList<string> Validate()
+    {
+        var errors = new List<string>();
+
+        // Validate QueryString - but we need a string parameter, so we'll test with a sample
+        var queryResult = ValidationHelper.ValidateQueryString("query { test }", out var queryErrors);
+        if (!queryResult)
+        {
+            errors.AddRange(queryErrors);
+        }
+
+        // Validate TypeName
+        if (!ValidationHelper.ValidateTypeName("TestType"))
+        {
+            errors.Add("Type name validation failed for 'TestType'");
+        }
+
+        if (!ValidationHelper.ValidateTypeName(""))
+        {
+            errors.Add("Type name validation failed for empty string");
+        }
+
+        if (ValidationHelper.ValidateTypeName("123Invalid"))
+        {
+            errors.Add("Type name validation should fail for name starting with digit");
+        }
+
+        // Validate FieldName
+        if (!ValidationHelper.ValidateFieldName("testField"))
+        {
+            errors.Add("Field name validation failed for 'testField'");
+        }
+
+        // Validate Email
+        if (!ValidationHelper.ValidateEmail("test@example.com"))
+        {
+            errors.Add("Email validation failed for valid email 'test@example.com'");
+        }
+
+        if (ValidationHelper.ValidateEmail("invalid-email"))
+        {
+            errors.Add("Email validation should fail for invalid email 'invalid-email'");
+        }
+
+        if (ValidationHelper.ValidateEmail(""))
+        {
+            errors.Add("Email validation should fail for empty email");
+        }
+
+        // Validate URL
+        if (!ValidationHelper.ValidateUrl("https://example.com"))
+        {
+            errors.Add("URL validation failed for valid URL 'https://example.com'");
+        }
+
+        if (ValidationHelper.ValidateUrl("not-a-url"))
+        {
+            errors.Add("URL validation should fail for invalid URL 'not-a-url'");
+        }
+
+        if (ValidationHelper.ValidateUrl(""))
+        {
+            errors.Add("URL validation should fail for empty URL");
+        }
+
+        // Validate ID
+        if (!ValidationHelper.ValidateId("12345"))
+        {
+            errors.Add("ID validation failed for numeric ID '12345'");
+        }
+
+        if (!ValidationHelper.ValidateId("550e8400-e29b-41d4-a716-446655440000"))
+        {
+            errors.Add("ID validation failed for valid GUID");
+        }
+
+        if (ValidationHelper.ValidateId(""))
+        {
+            errors.Add("ID validation should fail for empty ID");
+        }
+
+        // Validate ComplexityScore - requires maxScore parameter
+        if (!ValidationHelper.ValidateComplexityScore(5, 10))
+        {
+            errors.Add("Complexity score validation failed for valid score");
+        }
+
+        if (ValidationHelper.ValidateComplexityScore(-1, 10))
+        {
+            errors.Add("Complexity score validation should fail for negative score");
+        }
+
+        if (ValidationHelper.ValidateComplexityScore(11, 10))
+        {
+            errors.Add("Complexity score validation should fail for score exceeding max");
+        }
+
+        // Validate Depth - requires maxDepth parameter
+        if (!ValidationHelper.ValidateDepth(3, 10))
+        {
+            errors.Add("Depth validation failed for valid depth");
+        }
+
+        if (ValidationHelper.ValidateDepth(-1, 10))
+        {
+            errors.Add("Depth validation should fail for negative depth");
+        }
+
+        if (ValidationHelper.ValidateDepth(11, 10))
+        {
+            errors.Add("Depth validation should fail for depth exceeding max");
+        }
+
+        return errors.AsReadOnly();
+    }
+
+    /// <summary>
+    /// Checks if the ValidationHelper is valid (all validation methods pass)
+    /// </summary>
+    /// <returns>True if valid, false otherwise</returns>
+    public static bool IsValid()
+    {
+        var errors = Validate();
+        return errors.Count == 0;
+    }
+
+    /// <summary>
+    /// Ensures the ValidationHelper is valid, throwing ArgumentException if not
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when validation fails with list of problems</exception>
+    public static void EnsureValid()
+    {
+        var errors = Validate();
+
+        if (errors.Count > 0)
+        {
+            var errorMessage = new StringBuilder();
+            errorMessage.AppendLine("ValidationHelper validation failed:");
+
+            for (int i = 0; i < errors.Count; i++)
+            {
+                errorMessage.AppendLine($"  {i + 1}. {errors[i]}");
+            }
+
+            throw new ArgumentException(errorMessage.ToString());
+        }
+    }
+}
