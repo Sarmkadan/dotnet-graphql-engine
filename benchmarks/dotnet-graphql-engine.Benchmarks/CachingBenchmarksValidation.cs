b36e052dbe7799b6793643718f15fe354aebadc4
Vladyslav Zaiets <rutova2@gmail.com>
2023-01-17 08:30:35 +0200

    Input validation for CachingBenchmarks

--- benchmarks/dotnet-graphql-engine.Benchmarks/CachingBenchmarksValidation.cs
+++ benchmarks/dotnet-graphql-engine.Benchmarks/CachingBenchmarksValidation.cs
@@ -5,43 +5,40 @@ using System.Linq;
 namespace GraphQLEngine.Benchmarks;
 
 /// <summary>
-/// Validation helpers for CachingBenchmarks to ensure benchmark configuration is valid
+/// Provides validation methods for <see cref="CachingBenchmarks"/> instances to ensure benchmark configuration is valid.
 /// </summary>
 public static class CachingBenchmarksValidation
 {
     /// <summary>
-    /// Validates a CachingBenchmarks instance and returns a list of human-readable problems
+    /// Validates a <see cref="CachingBenchmarks"/> instance and returns a list of human-readable problems.
     /// </summary>
-    /// <param name="value">The CachingBenchmarks instance to validate</param>
-    /// <returns>List of validation problems; empty if valid</returns>
+    /// <param name="value">The <see cref="CachingBenchmarks"/> instance to validate.</param>
+    /// <returns>List of validation problems; empty if valid.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static IReadOnlyList<string> Validate(this CachingBenchmarks value)
     {
-        var problems = new List<string>();
+        ArgumentNullException.ThrowIfNull(value);
 
-        if (value == null)
-        {
-            problems.Add("CachingBenchmarks instance is null");
-            return problems.AsReadOnly();
-        }
+        var problems = new List<string>();
 
         // Validate that required fields are initialized after Setup() is called
         // These are checked via null checks in the benchmark methods themselves
-        if (value._serviceProvider == null)
+        if (value._serviceProvider is null)
         {
             problems.Add("_serviceProvider is not initialized. Ensure Setup() has been called.");
         }
 
-        if (value._executionService == null)
+        if (value._executionService is null)
         {
             problems.Add("_executionService is not initialized. Ensure Setup() has been called.");
         }
 
-        if (value._schemaService == null)
+        if (value._schemaService is null)
         {
             problems.Add("_schemaService is not initialized. Ensure Setup() has been called.");
         }
 
-        if (value._cachedQuery == null)
+        if (value._cachedQuery is null)
         {
             problems.Add("_cachedQuery is not initialized. Ensure Setup() has been called.");
         }
@@ -50,20 +47,18 @@ public static class CachingBenchmarksValidation
     }
 
     /// <summary>
-    /// Checks if a CachingBenchmarks instance is valid
+    /// Checks if a <see cref="CachingBenchmarks"/> instance is valid.
     /// </summary>
-    /// <param name="value">The CachingBenchmarks instance to check</param>
-    /// <returns>True if valid; false otherwise</returns>
-    public static bool IsValid(this CachingBenchmarks value)
-    {
-        return !value.Validate().Any();
-    }
+    /// <param name="value">The <see cref="CachingBenchmarks"/> instance to check.</param>
+    /// <returns><see langword="true"/> if valid; <see langword="false"/> otherwise.</returns>
+    public static bool IsValid(this CachingBenchmarks value) => !value.Validate().Any();
 
     /// <summary>
-    /// Ensures a CachingBenchmarks instance is valid, throwing ArgumentException if not
+    /// Ensures a <see cref="CachingBenchmarks"/> instance is valid, throwing <see cref="ArgumentException"/> if not.
     /// </summary>
-    /// <param name="value">The CachingBenchmarks instance to validate</param>
-    /// <exception cref="ArgumentException">Thrown when validation fails with list of problems</exception>
+    /// <param name="value">The <see cref="CachingBenchmarks"/> instance to validate.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown when validation fails with list of problems.</exception>
     public static void EnsureValid(this CachingBenchmarks value)
     {
         var problems = value.Validate();
