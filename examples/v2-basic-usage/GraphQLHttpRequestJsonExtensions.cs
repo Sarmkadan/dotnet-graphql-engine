63b734b24a50e2cf6d37b6a17fbfe2fc0bb7133f
Vladyslav Zaiets <rutova2@gmail.com>
2023-01-24 10:22:07 +0200

    feat(v2-basic-usage): json serialization for GraphQLHttpRequestJson

---
+++ examples/v2-basic-usage/GraphQLHttpRequestJsonExtensions.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public static class GraphQLHttpRequestJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+    /// <summary>
+    /// Converts the <see cref="GraphQLHttpRequest"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="GraphQLHttpRequest"/> to convert.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the <see cref="GraphQLHttpRequest"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this GraphQLHttpRequest value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        return JsonSerializer.Serialize(value, indented ? _jsonSerializerOptions with { WriteIndented = true } : _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="GraphQLHttpRequest"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A deserialized <see cref="GraphQLHttpRequest"/> or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static GraphQLHttpRequest? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        try
+        {
+            return JsonSerializer.Deserialize<GraphQLHttpRequest>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="GraphQLHttpRequest"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized <see cref="GraphQLHttpRequest"/> or null if deserialization fails.</param>
+    /// <returns>True if deserialization is successful; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out GraphQLHttpRequest? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        try
+        {
+            value = JsonSerializer.Deserialize<GraphQLHttpRequest>(json, _jsonSerializerOptions);
+            return value is not null;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
