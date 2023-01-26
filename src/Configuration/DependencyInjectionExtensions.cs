9296b5bed452b8fc45767d6200a319494650b156
Vladyslav Zaiets <rutova2@gmail.com>
2023-01-26 12:02:14 +0200

    small DependencyInjection helpers

--- src/Configuration/DependencyInjectionExtensions.cs
+++ src/Configuration/DependencyInjectionExtensions.cs
@@ -17,21 +17,30 @@ using Microsoft.Extensions.Options;
 namespace GraphQLEngine.Configuration;
 
 /// <summary>
-/// Extension methods for <see cref="DependencyInjection"/> class to provide additional configuration options
+/// Extension methods for configuring GraphQL engine services with various customization options.
 /// </summary>
 public static class DependencyInjectionExtensions
 {
     /// <summary>
-    /// Adds GraphQL engine services with custom logging configuration
+    /// Adds GraphQL engine services with custom logging configuration.
     /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="configureLogging">Optional logging configuration action</param>
-    /// <param name="configureOptions">Optional options configuration action</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configureLogging">Optional logging configuration action. If <see langword="null"/>, console logging with Information level is configured.</param>
+    /// <param name="configureOptions">Optional options configuration action for GraphQL engine.</param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/></exception>
+    /// <example>
+    /// <code>
+    /// services.AddGraphQLEngineWithLogging();
+    /// </code>
+    /// </example>
     public static IServiceCollection AddGraphQLEngineWithLogging(
         this IServiceCollection services,
         Action<ILoggingBuilder>? configureLogging = null,
         Action<GraphQLEngineOptions>? configureOptions = null)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         // Configure logging if provided
         if (configureLogging != null)
         {
@@ -53,14 +62,33 @@ public static class DependencyInjectionExtensions
     }
 
     /// <summary>
-    /// Adds GraphQL engine services with custom validation rules
+    /// Adds GraphQL engine services with custom validation rules.
     /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="validationAction">Action to configure validation rules</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="validationAction">Optional action to configure validation rules. Default values are:
+    /// <list type="bullet">
+    /// <item><description><see cref="GraphQLEngineOptions.MaxQueryComplexity"/> = 1000</description></item>
+    /// <item><description><see cref="GraphQLEngineOptions.MaxQueryDepth"/> = 5</description></item>
+    /// <item><description><see cref="GraphQLEngineOptions.QueryTimeoutMs"/> = 10000</description></item>
+    /// <item><description><see cref="GraphQLEngineOptions.EnableDetailedErrorMessages"/> = false</description></item>
+    /// </list>
+    /// </param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/></exception>
+    /// <example>
+    /// <code>
+    /// services.AddGraphQLEngineWithValidation(options => {
+    ///     options.MaxQueryComplexity = 2000;
+    ///     options.EnableDetailedErrorMessages = true;
+    /// });
+    /// </code>
+    /// </example>
     public static IServiceCollection AddGraphQLEngineWithValidation(
         this IServiceCollection services,
         Action<GraphQLEngineOptions>? validationAction = null)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.AddGraphQLEngine(options =>
         {
             // Apply default validation rules
@@ -77,14 +105,23 @@ public static class DependencyInjectionExtensions
     }
 
     /// <summary>
-    /// Adds GraphQL engine services with custom repository configuration
+    /// Adds GraphQL engine services with custom repository configuration.
     /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="repositoryLifetime">Lifetime for repository registrations</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="repositoryLifetime">The <see cref="ServiceLifetime"/> to use for repository registrations. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/></exception>
+    /// <example>
+    /// <code>
+    /// services.AddGraphQLEngineWithRepositoryLifetime(ServiceLifetime.Scoped);
+    /// </code>
+    /// </example>
     public static IServiceCollection AddGraphQLEngineWithRepositoryLifetime(
         this IServiceCollection services,
         ServiceLifetime repositoryLifetime = ServiceLifetime.Singleton)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.AddGraphQLEngine();
 
         // Re-register repositories with specified lifetime
@@ -122,14 +159,25 @@ public static class DependencyInjectionExtensions
     }
 
     /// <summary>
-    /// Adds GraphQL engine services with custom schema stitching configuration
+    /// Adds GraphQL engine services with custom schema stitching configuration.
     /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="schemaName">Name for the schema stitching configuration</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="schemaName">Name for the schema stitching configuration. Defaults to "default".</param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="schemaName"/> is <see langword="null"/></exception>
+    /// <example>
+    /// <code>
+    /// services.AddGraphQLEngineWithSchemaStitching("my-schema");
+    /// </code>
+    /// </example>
     public static IServiceCollection AddGraphQLEngineWithSchemaStitching(
         this IServiceCollection services,
         string schemaName = "default")
     {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(schemaName);
+
         services.AddGraphQLEngine();
 
         // Re-register schema stitching with custom name
