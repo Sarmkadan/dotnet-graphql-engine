e5ae3e64a519cc4d73966973e97a22ce02faa307
Vladyslav Zaiets <rutova2@gmail.com>
2023-02-07 20:51:36 +0200

    Oops
    also touched a couple of call sites.

---
+++ src/Common/Utilities/DateTimeExtensionsJsonExtensions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using GraphQLEngine.Common.Utilities;
+
+namespace GraphQLEngine.Common.Utilities
+{
+    /// <summary>
+    /// Provides JSON serialization and deserialization extensions for the <see cref="DateTimeExtensions"/> type.
+    /// </summary>
+    public static class DateTimeExtensionsJsonExtensions
+    {
+        /// <summary>
+        /// Private static readonly JSON serializer options with camelCase naming policy.
+        /// </summary>
+        private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+        {
+            WriteIndented = false,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        /// <summary>
+        /// Serializes a <see cref="DateTime"/> instance to a JSON string.
+        /// </summary>
+        /// <param name="value">The <see cref="DateTime"/> to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation.</param>
+        /// <returns>A JSON string representation of the <see cref="DateTime"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        public static string ToJson(this DateTime value, bool indented = false)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            
+            JsonSerializerOptions options = new(JsonSerializerOptions)
+            {
+                WriteIndented = indented
+            };
+            
+            return JsonSerializer.Serialize(value, options);
+        }
+
+        /// <summary>
+        /// Deserializes a JSON string to a <see cref="DateTime"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>A <see cref="DateTime"/> instance.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+        /// <exception cref="JsonException">Thrown when JSON deserialization fails.</exception>
+        public static DateTime? FromJson(string json)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(json);
+            
+            return JsonSerializer.Deserialize<DateTime>(json, JsonSerializerOptions);
+        }
+
+        /// <summary>
+        /// Tries to deserialize a JSON string to a <see cref="DateTime"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">The deserialized <see cref="DateTime"/> if successful.</param>
+        /// <returns>True if deserialization succeeded; otherwise false.</returns>
+        public static bool TryFromJson(string json, out DateTime? value)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(json);
+            
+            try
+            {
+                value = JsonSerializer.Deserialize<DateTime>(json, JsonSerializerOptions);
+                return value is not null;
+            }
+            catch (JsonException)
+            {
+                value = null;
+                return false;
+            }
+        }
+    }
+}
