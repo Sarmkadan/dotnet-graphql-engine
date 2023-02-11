deb9eddc247893b6e86e972a3f765f7d453a04a1
Vladyslav Zaiets <rutova2@gmail.com>
2023-02-11 15:24:08 +0200

    json serialization for StringExtensionsJsonExtensionsJson

---
+++ src/Common/Utilities/StringExtensionsJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+// =============================================================================
+// Author: 
+// =============================================================================
+
+using System;
+using System.Text.Json;
+
+namespace GraphQLEngine.Common.Utilities;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extension methods.
+/// </summary>
+public static class StringExtensionsJsonExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a string to its JSON string representation.
+    /// </summary>
+    /// <param name="value">The string value to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>The JSON string representation of the value.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this string value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a string value.
+    /// </summary>
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <returns>The deserialized string value, or <see langword="null"/> if the input is null, whitespace, or parsing fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static string? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+            return null;
+
+        try
+        {
+            return JsonSerializer.Deserialize<string>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a string value.
+    /// </summary>
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <param name="value">Output parameter for the deserialized value.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out string? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+            return false;
+
+        try
+        {
+            value = JsonSerializer.Deserialize<string>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
