23a9a10fb86498e38100dffc921ab8f4c4309da4
Vladyslav Zaiets <rutova2@gmail.com>
2023-02-14 18:50:56 +0200

    Json serialization for EventJson

---
+++ src/Services/Events/EventJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace GraphQLEngine.Services.Events;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for Event types
+/// </summary>
+public static class EventJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes an Event to a JSON string
+    /// </summary>
+    /// <param name="value">The event to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation</param>
+    /// <returns>A JSON string representation of the event</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    public static string ToJson(this Event value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes an Event from a JSON string
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>The deserialized Event, or null if the JSON is invalid</returns>
+    /// <exception cref="ArgumentException">Thrown when json is null or empty</exception>
+    public static Event? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<Event>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize an Event from a JSON string
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">The deserialized Event, or null if deserialization failed</param>
+    /// <returns>True if deserialization succeeded; false otherwise</returns>
+    /// <exception cref="ArgumentException">Thrown when json is null or empty</exception>
+    public static bool TryFromJson(string json, out Event? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<Event>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
