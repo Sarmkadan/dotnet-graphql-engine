17df6a3872a400faf68d3f38c96198c62f32059c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 13:12:48 +0000

    refactor(utilities): tighten guard clauses and docs in DateTimeExtensionsValidation

--- src/Common/Utilities/DateTimeExtensionsValidation.cs
+++ src/Common/Utilities/DateTimeExtensionsValidation.cs
@@ -18,176 +18,99 @@ public static class DateTimeExtensionsValidation
     /// </summary>
     /// <param name="dateTime">The DateTime value to validate</param>
     /// <returns>List of validation problems (empty if valid)</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when dateTime is MinValue or MaxValue</exception>
     public static IReadOnlyList<string> Validate(this DateTime dateTime)
     {
-        var problems = new List<string>();
+        ArgumentOutOfRangeException.ThrowIfEqual(dateTime, DateTime.MinValue, nameof(dateTime));
+        ArgumentOutOfRangeException.ThrowIfEqual(dateTime, DateTime.MaxValue, nameof(dateTime));
 
-        // Check for default DateTime (MinValue) which indicates uninitialized/unspecified date
-        if (dateTime == default)
-        {
-            problems.Add("DateTime value is default (uninitialized)");
-        }
+        var problems = new List<string>();
 
         // Validate Unix timestamp methods (indirect check via reasonable range)
         // Unix timestamps should be reasonable (not too far in past/future)
-        try
-        {
-            var unixTimestamp = dateTime.ToUnixTimestamp();
-            var minReasonableTimestamp = new DateTime(2000, 1, 1).ToUnixTimestamp();
-            var maxReasonableTimestamp = DateTime.UtcNow.AddYears(10).ToUnixTimestamp();
+        var unixTimestamp = dateTime.ToUnixTimestamp();
+        var minReasonableTimestamp = new DateTime(2000, 1, 1).ToUnixTimestamp();
+        var maxReasonableTimestamp = DateTime.UtcNow.AddYears(10).ToUnixTimestamp();
 
-            if (unixTimestamp < minReasonableTimestamp || unixTimestamp > maxReasonableTimestamp)
-            {
-                problems.Add("Unix timestamp is outside reasonable range");
-            }
-        }
-        catch
+        if (unixTimestamp < minReasonableTimestamp || unixTimestamp > maxReasonableTimestamp)
         {
-            problems.Add("Failed to convert to Unix timestamp");
+            problems.Add("Unix timestamp is outside reasonable range");
         }
 
         // Validate StartOfDay/EndOfDay - should be valid DateTime values
-        try
-        {
-            var startOfDay = dateTime.StartOfDay();
-            var endOfDay = dateTime.EndOfDay();
+        var startOfDay = dateTime.StartOfDay();
+        var endOfDay = dateTime.EndOfDay();
 
-            if (startOfDay > endOfDay)
-            {
-                problems.Add("StartOfDay is after EndOfDay");
-            }
-        }
-        catch
+        if (startOfDay > endOfDay)
         {
-            problems.Add("Failed to calculate StartOfDay or EndOfDay");
+            problems.Add("StartOfDay is after EndOfDay");
         }
 
         // Validate StartOfWeek/EndOfWeek - should be valid DateTime values
-        try
+        var startOfWeek = dateTime.StartOfWeek();
+        var endOfWeek = dateTime.EndOfWeek();
+
+        if (startOfWeek > endOfWeek)
         {
-            var startOfWeek = dateTime.StartOfWeek();
-            var endOfWeek = dateTime.EndOfWeek();
-
-            if (startOfWeek > endOfWeek)
-            {
-                problems.Add("StartOfWeek is after EndOfWeek");
-            }
-
-            // Week should be within reasonable bounds
-            var minWeek = new DateTime(2000, 1, 1).StartOfWeek();
-            var maxWeek = DateTime.UtcNow.AddYears(1).StartOfWeek();
-            if (startOfWeek < minWeek || startOfWeek > maxWeek)
-            {
-                problems.Add("StartOfWeek is outside reasonable range");
-            }
+            problems.Add("StartOfWeek is after EndOfWeek");
         }
-        catch
+
+        // Week should be within reasonable bounds
+        var minWeek = new DateTime(2000, 1, 1).StartOfWeek();
+        var maxWeek = DateTime.UtcNow.AddYears(1).StartOfWeek();
+        if (startOfWeek < minWeek || startOfWeek > maxWeek)
         {
-            problems.Add("Failed to calculate StartOfWeek or EndOfWeek");
+            problems.Add("StartOfWeek is outside reasonable range");
         }
 
         // Validate StartOfMonth/EndOfMonth - should be valid DateTime values
-        try
+        var startOfMonth = dateTime.StartOfMonth();
+        var endOfMonth = dateTime.EndOfMonth();
+
+        if (startOfMonth > endOfMonth)
         {
-            var startOfMonth = dateTime.StartOfMonth();
-            var endOfMonth = dateTime.EndOfMonth();
-
-            if (startOfMonth > endOfMonth)
-            {
-                problems.Add("StartOfMonth is after EndOfMonth");
-            }
-
-            // Month should be within reasonable bounds
-            var minMonth = new DateTime(2000, 1, 1).StartOfMonth();
-            var maxMonth = DateTime.UtcNow.AddYears(1).StartOfMonth();
-            if (startOfMonth < minMonth || startOfMonth > maxMonth)
-            {
-                problems.Add("StartOfMonth is outside reasonable range");
-            }
+            problems.Add("StartOfMonth is after EndOfMonth");
         }
-        catch
+
+        // Month should be within reasonable bounds
+        var minMonth = new DateTime(2000, 1, 1).StartOfMonth();
+        var maxMonth = DateTime.UtcNow.AddYears(1).StartOfMonth();
+        if (startOfMonth < minMonth || startOfMonth > maxMonth)
         {
-            problems.Add("Failed to calculate StartOfMonth or EndOfMonth");
+            problems.Add("StartOfMonth is outside reasonable range");
         }
 
         // Validate StartOfYear/EndOfYear - should be valid DateTime values
-        try
+        var startOfYear = dateTime.StartOfYear();
+        var endOfYear = dateTime.EndOfYear();
+
+        if (startOfYear > endOfYear)
         {
-            var startOfYear = dateTime.StartOfYear();
-            var endOfYear = dateTime.EndOfYear();
-
-            if (startOfYear > endOfYear)
-            {
-                problems.Add("StartOfYear is after EndOfYear");
-            }
-
-            // Year should be within reasonable bounds
-            var minYear = new DateTime(2000, 1, 1).StartOfYear();
-            var maxYear = DateTime.UtcNow.AddYears(10).StartOfYear();
-            if (startOfYear.Year < 2000 || startOfYear.Year > maxYear.Year)
-            {
-                problems.Add("StartOfYear is outside reasonable range");
-            }
+            problems.Add("StartOfYear is after EndOfYear");
         }
-        catch
+
+        // Year should be within reasonable bounds
+        var minYear = new DateTime(2000, 1, 1).StartOfYear();
+        var maxYear = DateTime.UtcNow.AddYears(10).StartOfYear();
+        if (startOfYear.Year < 2000 || startOfYear.Year > maxYear.Year)
         {
-            problems.Add("Failed to calculate StartOfYear or EndOfYear");
+            problems.Add("StartOfYear is outside reasonable range");
         }
 
         // Validate IsPast/IsFuture/IsToday - should not throw exceptions
-        try
-        {
-            _ = dateTime.IsPast();
-            _ = dateTime.IsFuture();
-            _ = dateTime.IsToday();
-        }
-        catch
-        {
-            problems.Add("Failed to evaluate IsPast/IsFuture/IsToday");
-        }
+        _ = dateTime.IsPast();
+        _ = dateTime.IsFuture();
+        _ = dateTime.IsToday();
 
         // Validate IsWithinPastDays/IsWithinFutureDays - should handle valid day ranges
-        try
-        {
-            _ = dateTime.IsWithinPastDays(7);
-            _ = dateTime.IsWithinFutureDays(7);
-        }
-        catch
-        {
-            problems.Add("Failed to evaluate IsWithinPastDays or IsWithinFutureDays");
-        }
+        _ = dateTime.IsWithinPastDays(7);
+        _ = dateTime.IsWithinFutureDays(7);
 
         // Validate ToHumanReadableTime - should not throw exceptions
-        try
-        {
-            _ = dateTime.ToHumanReadableTime();
-        }
-        catch
-        {
-            problems.Add("Failed to generate human readable time");
-        }
+        _ = dateTime.ToHumanReadableTime();
 
         // Validate ToISO8601 - should not throw exceptions
-        try
-        {
-            _ = dateTime.ToISO8601();
-        }
-        catch
-        {
-            problems.Add("Failed to format as ISO8601");
-        }
-
-        // Validate ParseISO8601 - should handle null/empty strings
-        try
-        {
-            _ = DateTimeExtensions.ParseISO8601(null);
-            _ = DateTimeExtensions.ParseISO8601(string.Empty);
-            _ = DateTimeExtensions.ParseISO8601("invalid-date");
-        }
-        catch
-        {
-            problems.Add("ParseISO8601 failed to handle invalid input");
-        }
+        _ = dateTime.ToISO8601();
 
         return problems.AsReadOnly();
     }
@@ -197,16 +120,16 @@ public static class DateTimeExtensionsValidation
     /// </summary>
     /// <param name="dateTime">The DateTime value to check</param>
     /// <returns>True if valid, false otherwise</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when dateTime is MinValue or MaxValue</exception>
     public static bool IsValid(this DateTime dateTime)
-    {
-        return Validate(dateTime).Count == 0;
-    }
+        => Validate(dateTime).Count == 0;
 
     /// <summary>
     /// Ensures a DateTime value is valid, throwing ArgumentException if not
     /// </summary>
     /// <param name="dateTime">The DateTime value to validate</param>
     /// <exception cref="ArgumentException">Thrown when validation fails with list of problems</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when dateTime is MinValue or MaxValue</exception>
     public static void EnsureValid(this DateTime dateTime)
     {
         var problems = Validate(dateTime);
