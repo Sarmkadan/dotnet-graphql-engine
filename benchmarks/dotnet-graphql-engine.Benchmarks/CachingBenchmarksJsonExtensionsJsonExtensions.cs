15a42bef00aa7be56a8bd799a217b9962603f368
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 09:13:52 +0300

    cleanup in CachingBenchmarksJsonExtensionsJsonExtensions

--- benchmarks/dotnet-graphql-engine.Benchmarks/CachingBenchmarksJsonExtensionsJsonExtensions.cs
+++ benchmarks/dotnet-graphql-engine.Benchmarks/CachingBenchmarksJsonExtensionsJsonExtensions.cs
@@ -4,10 +4,10 @@ using System.Text.Json;
 namespace GraphQLEngine.Benchmarks
 {
     /// <summary>
-    /// Provides JSON serialization and deserialization extensions for <see cref="CachingBenchmarksJsonExtensions"/>
+    /// Provides JSON serialization and deserialization extensions for <see cref="CachingBenchmarks"/>
     /// to enable caching benchmark configuration to be persisted or transmitted.
     /// </summary>
-    public static class CachingBenchmarksJsonExtensionsJsonExtensions
+    public static class CachingBenchmarksJsonExtensions
     {
         private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
         {
@@ -15,13 +15,13 @@ namespace GraphQLEngine.Benchmarks
         };
 
         /// <summary>
-        /// Serializes the <see cref="CachingBenchmarksJsonExtensions"/> instance to a JSON string.
+        /// Serializes the <see cref="CachingBenchmarks"/> instance to a JSON string.
         /// </summary>
-        /// <param name="value">The value to serialize.</param>
+        /// <param name="value">The <see cref="CachingBenchmarks"/> instance to serialize.</param>
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-        /// <returns>A JSON string representation of the value.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
-        public static string ToJson(this CachingBenchmarksJsonExtensions value, bool indented = false)
+        /// <returns>A JSON string representation of the <see cref="CachingBenchmarks"/> instance.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+        public static string ToJson(this CachingBenchmarks value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
 
@@ -33,12 +33,12 @@ namespace GraphQLEngine.Benchmarks
         }
 
         /// <summary>
-        /// Deserializes a JSON string to a <see cref="CachingBenchmarksJsonExtensions"/> instance.
+        /// Deserializes a JSON string to a <see cref="CachingBenchmarks"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <returns>The deserialized instance, or null if the JSON is null or empty.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
-        public static CachingBenchmarksJsonExtensions? FromJson(string json)
+        /// <returns>The deserialized <see cref="CachingBenchmarks"/> instance, or null if the JSON is null or empty.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+        public static CachingBenchmarks? FromJson(string json)
         {
             ArgumentNullException.ThrowIfNull(json);
 
@@ -47,17 +47,17 @@ namespace GraphQLEngine.Benchmarks
                 return null;
             }
 
-            return JsonSerializer.Deserialize<CachingBenchmarksJsonExtensions>(json, _options);
+            return JsonSerializer.Deserialize<CachingBenchmarks>(json, _options);
         }
 
         /// <summary>
-        /// Attempts to deserialize a JSON string to a <see cref="CachingBenchmarksJsonExtensions"/> instance.
+        /// Attempts to deserialize a JSON string to a <see cref="CachingBenchmarks"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <param name="value">Receives the deserialized instance if successful.</param>
+        /// <param name="value">Receives the deserialized <see cref="CachingBenchmarks"/> instance if successful.</param>
         /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
-        public static bool TryFromJson(string json, out CachingBenchmarksJsonExtensions? value)
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+        public static bool TryFromJson(string json, out CachingBenchmarks? value)
         {
             ArgumentNullException.ThrowIfNull(json);
 
@@ -70,7 +70,7 @@ namespace GraphQLEngine.Benchmarks
 
             try
             {
-                value = JsonSerializer.Deserialize<CachingBenchmarksJsonExtensions>(json, _options);
+                value = JsonSerializer.Deserialize<CachingBenchmarks>(json, _options);
                 return true;
             }
             catch (JsonException)
