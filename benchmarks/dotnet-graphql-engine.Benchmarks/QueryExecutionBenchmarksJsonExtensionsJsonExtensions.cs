989d654fae69e28cde1df022b9fd77b7e227acc0
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-29 06:27:31 +0000

    fix: correct namespace in benchmark file (hyphen to underscore)

--- benchmarks/dotnet-graphql-engine.Benchmarks/QueryExecutionBenchmarksJsonExtensionsJsonExtensions.cs
+++ benchmarks/dotnet-graphql-engine.Benchmarks/QueryExecutionBenchmarksJsonExtensionsJsonExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace dotnet-graphql-engine.Benchmarks
+namespace dotnet_graphql_engine.Benchmarks
 {
     public static class QueryExecutionBenchmarksJsonExtensions
     {
